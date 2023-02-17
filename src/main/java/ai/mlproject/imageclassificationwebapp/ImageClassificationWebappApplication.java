24e9b2cf80695edab3d5ed2fd08c15921163519d
andrewkimswe <andrewkimswe.gmail.com>
2023-02-17 16:37:16 +0900

    Initial commit

---
+++ src/main/java/ai/mlproject/imageclassificationwebapp/ImageClassificationWebappApplication.java
@@ -0,0 +1,13 @@
+package ai.mlproject.imageclassificationwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageClassificationWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageClassificationWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/mlproject/imageclassificationwebapp/ImageClassificationWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.mlproject.imageclassificationwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageClassificationWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
